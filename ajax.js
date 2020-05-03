6329f8e83fc2a31c08bd7d1aec657f6cd4d47667
Yak Fishman <yakfishman35@gmail.com>
2020-05-03 18:36:33 -0500

    Fixed api

--- ajax.js
+++ ajax.js
@@ -5,12 +5,17 @@ function getInstaPics(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            picurl = this.responseText;
-            document.getElementById("instapic1").setAttribute("src", picurl);
-            console.log(picurl);
+            info = JSON.parse(this.responseText);
+            picurl = info.html;
+            author_url = info.author_url;
+            console.log(author_url);
+            //document.getElementById("instapic1").setAttribute("src", picurl);
+            document.getElementById("instafeed").innerHTML = picurl;
+            instgrm.Embeds.process();
         }
     };
-    xhttp.open("GET", "https://instagram.com/p/B8zWbAwJrGx/media/?size=l", true);
+    xhttp.open("GET", "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B8zWbAwJrGx/", true);
+
     xhttp.send();
 }
 
